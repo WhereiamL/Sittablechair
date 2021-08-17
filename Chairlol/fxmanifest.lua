ec31ac7f85b927de8b27b5a91d45f9c392a306fe
WhereiamL <84282589+WhereiamL@users.noreply.github.com>
2021-08-17 13:01:34 +0200

    Update fxmanifest.lua

--- Chairlol/fxmanifest.lua
+++ Chairlol/fxmanifest.lua
@@ -3,9 +3,8 @@ game 'gta5'
 
 author 'WhereiamAKKAstifler'
 description 'My awesome 5$ Chair !'
-version '2.0.0'
+version '2.5.0'
 
-client_scripts {
 
-    'chair.lua',
-}
+client_scripts {'chair.lua'}
+server_scripts {'server.lua'}
